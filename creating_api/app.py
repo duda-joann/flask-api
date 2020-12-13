ae223ea73f149b969acd5ffb22978b87f2ddcde4
Joanna Duda <duda.joann@gmail.com>
2020-12-13 15:40:12 +0100

    adding api

--- creating_api/app.py
+++ creating_api/app.py
@@ -1,23 +1,32 @@
-import os
-import pathlib
-from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask import render_template
-from flask import request, jsonify
-from database_creation.app import db
 from database_creation.models import Artists
+from database_creation.create_db import app, db
 
 
-app = Flask(__name__)
 api = Api(app)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config.update(dict(DATABASE=os.path.join(pathlib.Path(app.root_path),
-                                        'database_creation', 'artists.db')))
-PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
 
-DATABASE = os.path.join(PROJECT_ROOT, 'database_creation', 'artists.db')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + DATABASE
-db.init_app(app)
+artists_put_args = reqparse.RequestParser()
+artists_put_args.add_argument("id", type=int, help="Artist ID", required=True)
+artists_put_args.add_argument("name", type=str, help="Name of the artist", required=True)
+artists_put_args.add_argument("playcount", type=str, help="Amount of listenings", required=True)
+artists_put_args.add_argument("listeners", type=int, help="Amount of the listeners", required=True)
+artists_put_args.add_argument("mbid", type=str, help="MBID number", required=True)
+
+artists_update_args = reqparse.RequestParser()
+artists_put_args.add_argument("id", type=int, help="Artist ID", required=True)
+artists_update_args.add_argument("name", type=str, help="Name of the artist is required", required=True)
+artists_update_args.add_argument("playcount", type=str, help="Amount of the listenings")
+artists_update_args.add_argument("likes", type=int, help="Likes on the video")
+artists_put_args.add_argument("mbid", type=str, help="MBID number")
+
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'playcount': fields.Integer,
+    'listeners': fields.Integer,
+    'mbid': fields.String,
+}
 
 @app.route('/', methods=['GET'])
 def render_main():
@@ -26,12 +35,75 @@ def render_main():
     return render_template('main.html')
 
 
-@app.route('/api/v1/all-artists', methods=['GET'])
-def get_all():
-    items = Artists.query.all()
-    items_json = jsonify(items)
-    return render_template('all.html', items=items_json)
+class ArtistAllResult(Resource):
+    @marshal_with(resource_fields)
+    def get(self):
+        result = Artists.query.all()
+        if not result:
+            abort(404, message="Could not find any data")
 
+        return result
+
+class ArtistIDApi(Resource):
+    @marshal_with(resource_fields)
+    def get(self, id):
+        result = Artists.query.filter_by(id=id).first()
+        if not result:
+            abort(404, message="Could not find artist with that id")
+        return result
+
+    @marshal_with(resource_fields)
+    def put(self, id):
+        args = artists_put_args.parse_args()
+        result = Artists.query.filter_by(id=id).first()
+        if result:
+            abort(409, message="Artist id taken...")
+
+        artist = Artists(
+            id=id,
+            name=args['name'],
+            playcount=args['playcount'],
+            listeners=args['listeners'],
+            mbid=args['mbid'])
+
+        db.session.add(artist)
+        db.session.commit()
+        return artist, 201
+
+    @marshal_with(resource_fields)
+    def delete(self, id):
+        query = Artists.delete(id=id)
+        if query:
+            query.execute()
+        else:
+            return f'Record does not exist', 405
+
+        db.session.commit()
+        return f'', 204
+
+    @marshal_with(resource_fields)
+    def patch(self, id):
+        args = artists_update_args.parse_args()
+        result = Artists.query.filter_by(id=id).first()
+        if not result:
+            abort(404, message="Video doesn't exist, cannot update")
+
+        if args['name']:
+            result.name = args['name']
+        if args['playcount']:
+            result.playcount = args['playcount']
+        if args['listeners']:
+            result.likes = args['listeners']
+        if args['mbid']:
+            result.likes = args['mbid']
+
+        db.session.commit()
+
+        return result
+
+
+api.add_resource(ArtistIDApi, "/api/v1/artist/<int:id>")
+api.add_resource(ArtistAllResult, "/api/v1/artists")
 
 if __name__ == '__main__':
     app.config["DEBUG"] = True
