ff36cf0bb5b6ef306e20613ee23412cda1eacce0
Joanna Duda <duda.joann@gmail.com>
2020-12-15 00:19:29 +0100

    first tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,12 @@
 import pytest
-import config
-
-from database_creation.create_db import create_app
-from database_creation.app import db
-from database_creation.models import Artists
+from database_creation.create_db import create_app, create_database
 
 
+@pytest.fixture
+def create_test_app():
+    app = create_app('testing')
+    create_database()
 
+    yield app
 
-
+    app.config['DB_FILE_PATH'].unlink(missing_ok=True)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,14 @@
 import pytest
 import requests
-import tempfile
-from creating_api.app import app
+from flask import Flask
+
 
 
 url = 'http://127.0.0.5000'
 
-@pytest.fixture()
-
+def test_app(app):
+    assert isinstance(app, Flask)
+    assert app.config['Testing ']
 
 def test_render_main():
     result = requests.get(url+'/')
@@ -18,6 +19,8 @@ def test_api_artistapidata():
     result = requests.get(url + '/api/v1/artist/1')
     assert result.status_code == 200
 
+
 def test_api_all_artists():
     result = requests.get(url+'def test_api')
     assert result.status_code == 200
+
