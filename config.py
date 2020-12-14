0e9a5a11a1631e08a49c6315ac08030fba9cfe50
Joanna Duda <duda.joann@gmail.com>
2020-12-14 23:53:57 +0100

    repair config

--- config.py
+++ config.py
@@ -1,28 +1,23 @@
 import os
 from pathlib import Path
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 base_dir = Path(__file__).resolve().parent
 env_file = base_dir / '.env'
-load_dotenv(env_file)
+#load_dotenv(env_file)
+
 
 class Config:
     DEBUG = True
-    SQLALCHELMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE')
+    SQLALCHELMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(Config):
-    DB_FILE_PATH = base_dir / 'database_creation'/ 'artists.db'
-    SQLALCHELMY_DATABASE_URI = f'sqlite:///{DB_FILE_PATH}'
     DEBUG = True
-    TESTING = True
-
 
 
 class TestingConfig(Config):
-    DB_FILE_PATH = base_dir /'tests'/ 'test.db'
-    SQLALCHELMY_DATABASE_URI = f'sqlite:///{DB_FILE_PATH}'
     DEBUG = True
     TESTING = True
 
@@ -31,3 +26,4 @@ config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig
 }
+
--- database_creation/create_db.py
+++ database_creation/create_db.py
@@ -7,8 +7,8 @@ from config import config
 def create_app(config_name='development'):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
-    """ app.config.update(dict(DATABASE=os.path.join(app.root_path, 'artists.db')))
-        app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///artists.db" """
+    app.config.update(DATABASE=os.path.join(app.root_path, 'artists.db'))
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///artists.db"
     db.init_app(app)
 
     with app.app_context():
